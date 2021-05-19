aa694f9ca3780515d09bd21a0e6bfb6c9a7e0b4f
ApolloXY <82376203+apollo-xy@users.noreply.github.com>
2021-05-19 23:26:11 +0530

    Update fullscreen.js

--- contact/fullscreen.js
+++ contact/fullscreen.js
@@ -23,3 +23,22 @@ function closeFullscreen() {
     
   }  
 }
+var checkBoxLoc = document.getElementById("loc");
+function getLocation() {
+   
+    if (checkBoxLoc.checked == true){
+    navigator.geolocation.getCurrentPosition(show_map, handle_error);  
+    }     
+  }
+  function show_map(position) {
+    $('#latitude').html(position.coords.latitude);
+    $('#longitude').html(position.coords.longitude);
+   // alert("Latitude: " + position.coords.latitude +"<br>Longitude: " + position.coords.longitude);
+  }
+  function handle_error(err) {
+    if (err.code == 1) {
+
+      checkBoxLoc.checked = false;
+    }
+  }
+
