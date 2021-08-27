47e49ed4a20e12c24fae557a50a25ec5759f5258
ApolloXY <82376203+apollo-xy@users.noreply.github.com>
2021-08-27 12:33:19 +0530

    Update copy.js

--- image_compressor/scripts/copy.js
+++ image_compressor/scripts/copy.js
@@ -1,37 +1,23 @@
 window.addEventListener('paste', (event)=>{
-    //document.onpaste = function(event){
-        var items = (event.clipboardData || event.originalEvent.clipboardData).items;
-        console.log(JSON.stringify(items)); // will give you the mime types
-        var blob = items[0].getAsFile();
-        var reader = new FileReader();
-        reader.fileName = "image_"+new Date().getTime();
-        reader.onload = function(event){
+  var items = (event.clipboardData || event.originalEvent.clipboardData).items; 
+  for(var i=0; i<items.length; i++){
+    var blob = items[i].getAsFile();
+    var reader = new FileReader();
+    reader.fileName = "image_"+i+new Date().getTime();
+    reader.readAsDataURL(blob);
+    reader.onload = function(event){
         
-        compressImg(event.target.result, function(result){
- 
-            document.getElementById("dwZip").style.display = "inline-block";
+    compressImg(event.target.result, function(result){
+      document.getElementById("dwZip").style.display = "inline-block";
 
-        var div = document.createElement("div");
-        div.innerHTML = "<div id='out'><img src='" +  result.url + "'/><p>" + event.target.fileName + ".png</p><p>" + result.oSize+"kb ➤ "+ result.cSize +"kb</p><br>" + "<button onclick='dL(this);'" + "fname='" + event.target.fileName + "' url='" + result.url + "'>Download</button><button id='remove' onclick='reMov(this);' fname='ApolloXY-"+ event.target.fileName  +".png'>❌</button>" +"</div>";
-        document.getElementById("out-main").insertBefore(div, null);
-        zip.folder("images").file("ApolloXY-"+event.target.fileName+".png", result.base64, {base64: true});
+      var div = document.createElement("div");
+      div.innerHTML = "<div id='out'><img src='" +  result.url + "'/><p>" + event.target.fileName + ".png</p><p>" + result.oSize+"kb ➤ "+ result.cSize +"kb</p><br>" + "<button onclick='dL(this);'" + "fname='" + event.target.fileName + "' url='" + result.url + "'>Download</button><button id='remove' onclick='reMov(this);' fname='ApolloXY-"+ event.target.fileName  +".png'>❌</button>" +"</div>";
+      document.getElementById("out-main").insertBefore(div, null);
+      zip.folder("images").file("ApolloXY-"+event.target.fileName+".png", result.base64, {base64: true});
        
             
-        });
-        }
-        
-        reader.readAsDataURL(blob);
       });
-      
-      
-      function loadImage(dataURL) {
-        // load image from data url
-        var img = new Image();
-        img.onload = function() {
-     
-        };
-      
-        img.src = dataURL;
-      }
-      
-    
+    }
+  }
+        
+});
