8932309727c1c5ef7257ffbccc50a2bafc5ab49b
ApolloXY <82376203+apollo-xy@users.noreply.github.com>
2021-08-31 18:27:40 +0530

    Update scripts.js

--- image_compressor/scripts/scripts.js
+++ image_compressor/scripts/scripts.js
@@ -169,6 +169,26 @@ function download(){
         saveAs(content, "Compressed_Images_ApolloXY.zip");
     });
   }
+
+  var topBtn = document.getElementById("top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
   window.onbeforeunload = confirmExit;
   function confirmExit() {
       return "You have attempted to leave this page. Are you sure?";
